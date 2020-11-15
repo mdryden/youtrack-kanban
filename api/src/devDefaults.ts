c102d66f781f6ae77823de9fb3fa58a7c65b80f6
Michael Dryden <mjdryden@gmail.com>
2020-11-15 14:29:53 -0600

    Change to typescript

--- api/src/devDefaults.ts
+++
@@ -1,8 +0,0 @@
-export const backlogQuery =
-  "in:%20PRIME%20AND%20((Kanban%20Stage:%20Backlog)%20or%20(Kanban%20Stage:%20Validate%20and%20Kanban%20State:%20-Done))";
-
-export const designValidationQuery = "";
-export const implementQuery = "";
-export const validateQuery = "";
-export const readyQuery = "";
-export const doneQuery = "";
--- api/src/server.ts
+++
@@ -1,9 +0,0 @@
-import * as Router from "koa-router";
-import IndexController from "./controllers/index.controller";
-
-const router = new Router();
-
-router.get("/", IndexController.getIndex);
-router.get("/backlog", IndexController.getBackLog);
-
-export default router;
